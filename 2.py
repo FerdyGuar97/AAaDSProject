68ce81d317523c83426dacbaf54e70457d4147fc
FerdyGuar97 <ferdy97@hotmail.it>
2019-12-30 18:41:26 +0100

    Chibencomincia
    è a metà dell'opera

--- 2.py
+++ 2.py
@@ -1,5 +1,19 @@
 
-from TdP_collections.priority_queue.adaptable_heap_priority_queue import AdaptableHeapPriorityQueue
+import Producer
+import time as ciccio
+from Process import Process
 
-queue = AdaptableHeapPriorityQueue()
+def test():
+    Producer.readAndPrint()
+    queue = Producer.load("ciccio")
 
+    while not queue.is_empty():
+        p=queue.remove_min()
+        processPriority = p[0]
+        processName = p[1].getName()
+        processSlices = p[1].getTimeSlices()
+        print(processName, processPriority, processSlices)
+        ciccio.sleep(1)
+
+if __name__ == '__main__':
+    test()
--- Producer.py
+++ Producer.py
@@ -1,13 +1,28 @@
+from Process import Process
+from TdP_collections.priority_queue.adaptable_heap_priority_queue import AdaptableHeapPriorityQueue
 
-
-def ReadAndPrint():
+def readAndPrint():
     file = open("ciccio", "r")
 
     for line in file:
-        tmp = line.split(" ")
+        tmp = line.rstrip("\n").split(" ")
         scheduler_process = tmp[0]
         scheduler_priority = tmp[1]
         scheduler_lenght = tmp[2]
         scheduler_arrive = tmp[3]
         print(scheduler_process, scheduler_priority, scheduler_lenght, scheduler_arrive)
 
+def load(fileName: str):
+    file = open(fileName, "r")
+    queue = AdaptableHeapPriorityQueue()
+
+    for line in file:
+        tmp = line.rstrip("\n").split(" ")
+        scheduler_process = tmp[0]
+        scheduler_priority = int(tmp[1])
+        scheduler_lenght = int(tmp[2])
+        scheduler_arrive = int(tmp[3])
+        p = Process(scheduler_process, scheduler_priority, scheduler_lenght)
+        queue.add(p.getPriority(), p)
+
+    return queue
