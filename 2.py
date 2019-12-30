875fda0c5f893eb20a1c4afdff91b7a1932fa15c
Salvatore Gravina <salvatore.gravina16@gmail.com>
2019-12-30 19:14:10 +0100

    loop
    ciao

--- 2.py
+++ 2.py
@@ -1,19 +1,29 @@
 
 import Producer
-import time as ciccio
-from Process import Process
+import time
+import Process
 
 def test():
     Producer.readAndPrint()
     queue = Producer.load("ciccio")
 
-    while not queue.is_empty():
-        p=queue.remove_min()
-        processPriority = p[0]
-        processName = p[1].getName()
-        processSlices = p[1].getTimeSlices()
-        print(processName, processPriority, processSlices)
-        ciccio.sleep(1)
+    actProcess = queue.remove_min()[1]
+    remainingTimeSlices = actProcess.getTimeSlices()
+
+    while True:
+        time.sleep(2)
+        if actProcess== None:
+            print("No job running")
+        else:
+            print("Executing " + actProcess.getName())
+            remainingTimeSlices -= 1
+        if not queue.is_empty() and remainingTimeSlices<=0:
+            actProcess = queue.remove_min()[1]
+            remainingTimeSlices = actProcess.getTimeSlices()
+        elif remainingTimeSlices<=0:
+            actProcess= None
+
+
 
 if __name__ == '__main__':
     test()
