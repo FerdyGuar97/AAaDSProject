e59b1ffa7fe70b73034ba950f38e85c694e972ce
FerdyGuar97 <ferdy97@hotmail.it>
2019-12-30 17:28:09 +0100

    Adjustamento

--- Producer.py
+++ Producer.py
@@ -1,10 +1,13 @@
 
-f = open("ciccio", "r")
 
-for line in f:
-    tmp = line.split(" ")
-    scheduler_process = tmp[0]
-    scheduler_priority = tmp[1]
-    scheduler_lenght = tmp[2]
-    print(scheduler_process, scheduler_priority, scheduler_lenght)
+def producer():
+    file = open("ciccio", "r")
+
+    for line in file:
+        tmp = line.split(" ")
+        scheduler_process = tmp[0]
+        scheduler_priority = tmp[1]
+        scheduler_lenght = tmp[2]
+        scheduler_arrive = tmp[3]
+        print(scheduler_process, scheduler_priority, scheduler_lenght)
 
