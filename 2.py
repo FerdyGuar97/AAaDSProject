ec22ea9a6e92ba8227b35c5880c89a57995b1e23
FerdyGuar97 <ferdy97@hotmail.it>
2020-01-02 16:29:06 +0100

    Funzia
    bene

--- 2.py
+++ 2.py
@@ -5,16 +5,18 @@ import Process
 
 
 def test(timeslice: int):
+
     Producer.readAndPrint()
     queue, loc = Producer.load("ciccio")
     actProcess = queue.remove_min()[1]
     remainingTimeSlices = actProcess.timeSlices
+
     while True:
         time.sleep(2)
         if actProcess is None:
             print("No job running")
         else:
-            print("Executing " + actProcess.name+"\n")
+            print("Executing " + actProcess.name)
             remainingTimeSlices -= 1
         if not queue.is_empty() and remainingTimeSlices <= 0:
             actProcess = queue.remove_min()[1]
@@ -24,16 +26,26 @@ def test(timeslice: int):
         queueupdate(loc, queue, timeslice)
 
 
-def queueupdate(loc, queue, timeslice):
-    for x in loc:
-        loc[x]+=1
-        if loc[x] >= timeslice:
-            x._value.priority -= 1
+def queueupdate(waitingTimesMap, queue, maxWaitingTime):
+
+    toRemove = []
+
+    for locator in waitingTimesMap:
+        waitingTimesMap[locator]+=1
+        if waitingTimesMap[locator] >= maxWaitingTime:
+            if locator._value.priority >= -19:
+                locator._value.priority -= 1
             try:
-                queue.update(x, x._value.priority, x._value)
-                loc[x]=0
+                queue.update(locator, locator._value.priority, locator._value)
+                waitingTimesMap[locator] = 0
             except ValueError as error:
-                print("something")
+                toRemove.append(locator)
+    print(waitingTimesMap)
+    print("\n")
+
+    for x in toRemove:
+        waitingTimesMap.pop(x)
+
 
 if __name__ == '__main__':
     test(2)
