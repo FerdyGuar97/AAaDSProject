fd6ce549cc0c197acdc816dd91487a83674338e5
KillEmFlacko <KillEmFlacko@users.noreply.github.com>
2020-01-05 19:02:39 +0100

    Exercise 4: Rendendolo più incasinato e non riconoscibile

--- Exercise4/bacefook_dynamic_algorithm.py
+++ Exercise4/bacefook_dynamic_algorithm.py
@@ -11,14 +11,14 @@ def bacefook_minimum_softwares(tree: GeneralTree, root: GeneralTree.Position, be
     # installato su di esso (best[c][0])
 
     for c in tree.children(root):
-        best[c][False], best[c][True] = bacefook_minimum_softwares(tree, c, best)
+        best[c][True], best[c][False] = bacefook_minimum_softwares(tree, c, best)
 
-    withoutRoot = sum(best[c][True] for c in tree.children(root))
-    withRoot = 1 + sum(min(best[c][False], best[c][True]) for c in tree.children(root))
-    return withoutRoot, withRoot
+    yes_software = sum(min(best[c][False], best[c][True]) for c in tree.children(root))
+    no_software = sum(best[c][True] for c in tree.children(root))
+    return yes_software + 1, no_software
 
 
 def bacefook_dynamic_algorithm(tree: GeneralTree):
-    best = {i: {False: 0, True: 0} for i in tree.postorder()}
-    best[tree.root()][False], best[tree.root()][True] = bacefook_minimum_softwares(tree, tree.root(), best)
+    best = {i: {True: 0, False: 0} for i in tree.preorder()}
+    best[tree.root()][True], best[tree.root()][False] = bacefook_minimum_softwares(tree, tree.root(), best)
     return best
